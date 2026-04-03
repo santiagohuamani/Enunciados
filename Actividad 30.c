47b2b3c5e9712d7536af0b016482bcd52746ae89
santiagohuamani <santiagohuamani130@gmail.com>
2026-04-03 15:09:21 -0300

    Create Actividad 30.c

---
+++ Actividad 30.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,fact=1;
+
+    printf("Numero: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    fact=fact*i;
+
+    printf("Factorial %d",fact);
+
+    return 0;
+}
