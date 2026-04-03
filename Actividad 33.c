798eb112d7d525327e9aa2df625ff456b8934651
santiagohuamani <santiagohuamani130@gmail.com>
2026-04-03 15:30:04 -0300

    Create Actividad 33.c

---
+++ Actividad 33.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i,producto;
+
+    printf("Numero 1: ");
+    scanf("%d",&a);
+
+    printf("Numero 2: ");
+    scanf("%d",&b);
+
+    for(i=1;i<=b;i++)
+    producto=producto+a;
+
+    printf("Producto %d",producto);
+
+    return 0;
+}
